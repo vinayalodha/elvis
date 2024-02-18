95e4116323c32e7e3c94ea47b81989ffe8c88237
vinay <vinay.a.lodha@gmail.com>
2024-02-18 11:15:50 +0100

    test

--- plugin/src/main/java/io/github/vinayalodha/elvis/plugin/constants/ErrorMessage.java
+++ plugin/src/main/java/io/github/vinayalodha/elvis/plugin/constants/ErrorMessage.java
@@ -8,7 +8,7 @@ import io.github.vinayalodha.elvis.annotation.NullSafe;
  */
 public interface ErrorMessage {
 
-    String NULL_SAFE_CODE = String.format("Expression is already null safe, Are you sure you need of %s annotation", NullSafe.class.getSimpleName());
+    String NULL_SAFE_CODE = String.format(" is already null safe, Are you sure you need of %s annotation", NullSafe.class.getSimpleName());
     String PARSING_BUG = String.format("Unable to parse expression, for applying @%s. please raise a bug", NullSafe.class.getSimpleName());
     String NO_EL_CONVERTER = String.format("Unable to parse value parameter passed in %s", NullSafe.class.getSimpleName());
     String BINARY_EXPRESSION = String.format("Ambiguous expression, @%s can not be applied", NullSafe.class.getSimpleName());
