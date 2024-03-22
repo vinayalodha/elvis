43dfc6bed29f1de3b86c3f610225911de53e62c8
vinay <vinay.a.lodha@gmail.com>
2024-03-22 21:27:24 +0100

    test

--- plugin-test/src/test/java/io/github/vinayalodha/elvis/plugin/test/positive/StringTest.java
+++ plugin-test/src/test/java/io/github/vinayalodha/elvis/plugin/test/positive/StringTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test;
 public class StringTest {
     @Test
     public void test_default_value() {
+        var vishal = getNull().trim();
+
         @NullSafe
         var vinay = getNull().trim();
         Assertions.assertEquals(null, vinay);
